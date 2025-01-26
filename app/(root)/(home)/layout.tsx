01878bdc3ce1a4228b1255d91d268d1b635e24e6
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-01-26 21:04:51 +0530

    edited the layout of Home.

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -22,7 +22,7 @@ const HomeLayout = ({children}: LayoutProps) => {
         <Navbar />
         <div className='flex'>
             <Sidebar />
-            <section className='flex flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+            <section className='flex flex-1 min-h-screen px-6 pb-6 pt-[100px] max-md:pb-14 sm:px-14'>
                 <div className='w-full'>
                     {children}
                 </div>
