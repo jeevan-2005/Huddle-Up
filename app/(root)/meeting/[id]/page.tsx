861ea5f344c0f050a24132b46942e413d55f45ca
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-01-26 20:47:06 +0530

    modified the params.

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,29 +7,27 @@ import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
-// the params field is provided as part of the server-side rendering context, and its type is usually a Promise because it's dynamic and might involve async operations during build or runtime. so use Awaited<> .
-type Props = {
-  params: Awaited<{
-    id: string;
-  }>;
-};
-
-const Meeting = ({ params }: Props) => {
+const Meeting = ({ params }: { params: { id: string } }) => {
   const { isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading} = useGetCallById(params.id);
+  const { call, isCallLoading } = useGetCallById(params.id);
 
-  if(!isLoaded || isCallLoading) return (
-    <div className="h-screen flex items-center justify-center w-full">
-      <Loader />
-    </div>
-  )
+  if (!isLoaded || isCallLoading)
+    return (
+      <div className="h-screen flex items-center justify-center w-full">
+        <Loader />
+      </div>
+    );
 
   return (
     <main>
       <StreamCall call={call}>
         <StreamTheme>
-          {isSetupComplete ? <MeetingRoom /> : <MeetingSetup setIsSetupComplete={setIsSetupComplete} />}
+          {isSetupComplete ? (
+            <MeetingRoom />
+          ) : (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
